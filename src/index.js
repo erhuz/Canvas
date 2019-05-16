8a47e8f495c59b3832680c71d46415afef919054
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-05-16 21:45:05 +0200

    Added Player sprite sheet and animation. also added controlls for jumping and walking

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Hero from "./assets/Characters/player/player.png";
+import Atlas from "./assets/Characters/player/player.json";
 
 const config = {
   type: Phaser.AUTO,
@@ -8,25 +9,104 @@ const config = {
   height: 600,
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update,
   }
 };
-
 const game = new Phaser.Game(config);
 
+let player;
+let cursors;
+
+
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.atlas("player", Hero, Atlas);
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
+  player = this.add.sprite(100, 300, "player", "Idle/0001.png" );
+
+
+  this.anims.create({
+    key: "idle",
+    frameRate: 4,
+    repeat: -1,
+    frames: this.anims.generateFrameNames('player', {
+      prefix: "Idle/",
+      suffix: ".png",
+      start: 1,
+      end: 5,
+      zeroPad: 4,
+    }),
+  })
+
+  this.anims.create({
+    key: "right",
+    frameRate: 10,
+    repeat: -1,
+    frames: this.anims.generateFrameNames('player', {
+      prefix: "Walk/",
+      suffix: ".png",
+      start: 1,
+      end: 5,
+      zeroPad: 4,
+    }),
   });
+
+  this.anims.create({
+    key: "jump",
+    frameRate: 5,
+    repeat: -1,
+    frames: this.anims.generateFrameNames('player', {
+      prefix: "Jump/",
+      suffix: ".png",
+      start: 1,
+      end: 5,
+      zeroPad: 4,
+    }),
+  });
+
+  this.anims.create({
+    key: "hurt",
+    frameRate: 10,
+    repeat: -1,
+    frames: this.anims.generateFrameNames('player', {
+      prefix: "Hurt/",
+      suffix: ".png",
+      start: 1,
+      end: 5,
+      zeroPad: 4,
+    }),
+  });
+
+  this.anims.create({
+    key: "die",
+    frameRate: 5,
+    repeat: -1,
+    frames: this.anims.generateFrameNames('player', {
+      prefix: "Die/",
+      suffix: ".png",
+      start: 1,
+      end: 5,
+      zeroPad: 4,
+    }),
+  });
+cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+  if (cursors.right.isDown) {
+    player.anims.play("right", true);
+  }
+  else if (cursors.left.isDown) {
+    player.anims.play("left", true);
+  }
+  else if (cursors.up.isDown) {
+    player.anims.play("jump", true)
+  }
+  else {
+    player.anims.play("idle", true)
+  }
+  
 }
