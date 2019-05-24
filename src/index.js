3812fef43761b8b6b407efa8405a030c12ea95c4
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-05-24 16:21:17 +0200

    Added some more objects to the world. also section of some of the code with comments

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 300 },
+      gravity: { y: 800 },
       debug: false
     }
   },
@@ -56,14 +56,15 @@ function create() {
   this.add.image(250, 260, 'background-5').setScale(3.5);
 
   // Create player
-  player = this.physics.add.sprite(100, 496, 'player', 'Idle/0001.png');
+  // player = this.physics.add.sprite(100, 496, 'player', 'Idle/0001.png').setScale(.7);
+  player = this.physics.add.sprite(700, 96, 'player', 'Idle/0001.png').setScale(.7);
   player.setCollideWorldBounds(true);
   this.cameras.main.startFollow(player, true, 0.08, 0.1);
-  
+
   // Set world Bounds
   this.physics.world.setBounds(-220, -60, 30392, 6000); // Bounds expand downwards, counter that by moving Y accordingly
-  
-  
+
+
   /*======================
         CREATE WORLD
   ========================*/
@@ -72,31 +73,61 @@ function create() {
   obstacles = this.physics.add.staticGroup();
   roofs = this.physics.add.staticGroup();
 
-  //Ground
+  /*======================
+          GROUND
+  ========================*/
   platforms.create(1200, 568, 'ground').setScale(8, 2).refreshBody();
 
-  // Roof
+  /*======================
+          ROOFS
+  ========================*/
   roofs.create(50, 0, 'platform').setScale(1, .4).refreshBody();
   roofs.create(350, 0, 'platform').setScale(1, .4).refreshBody();
+  roofs.create(650, 0, 'platform').setScale(1, .4).refreshBody();
+  roofs.create(950, 0, 'platform').setScale(1, .4).refreshBody();
 
-  // Walls
-  walls.create(0, 472, 'wall').setScale(.4, .42).refreshBody();
+  /*======================
+      WALLS
+  ========================*/
+  walls.create(0, 385, 'wall').setScale(.4, 1).refreshBody();
+  walls.create(0, 110, 'wall').setScale(.4, .8).refreshBody();
   walls.create(180, 385, 'wall').setScale(.4, 1).refreshBody();
-  walls.create(0, 250, 'wall').setScale(.4, 1).refreshBody();
+  walls.create(180, 185, 'wall').setScale(.4, .5).refreshBody();
+  walls.create(745, 385, 'wall').setScale(.4, .5).refreshBody();
+  walls.create(745, 85, 'wall').setScale(.4, .5).refreshBody();
 
-  // Platforms
-  platforms.create(30, 400, 'platform').setScale(.3).refreshBody();
-  platforms.create(150, 240, 'platform').setScale(.3).refreshBody();
-
-  platforms.create(950, 380, 'platform').setScale(.4).refreshBody();
+  /*======================
+      PLATFORMS
+  ========================*/
+  platforms.create(150, 400, 'platform').setScale(.3).refreshBody();
+  platforms.create(30, 240, 'platform').setScale(.3).refreshBody();
   platforms.create(314, 390, 'platform').setScale(1, .4).refreshBody();
-  
-  
-  // Dangerous Obstacles
-  obstacles.create(310, 365, 'obstacle');
-  obstacles.create(339, 365, 'obstacle');
+  platforms.create(610, 390, 'platform').setScale(1, .4).refreshBody();
+  platforms.create(500, 150, 'platform').setScale(1, .4).refreshBody();
+  platforms.create(610, 150, 'platform').setScale(1, .4).refreshBody();
 
-  // Colliders
+  /*======================
+      DANGEROUS OBSTACLES
+  ========================*/
+
+  // FIRST PLATFORM
+  obstacles.create(220, 365, 'obstacle');
+  obstacles.create(249, 365, 'obstacle');
+  obstacles.create(278, 365, 'obstacle');
+  obstacles.create(400, 365, 'obstacle');
+  obstacles.create(429, 365, 'obstacle');
+  obstacles.create(458, 365, 'obstacle');
+  obstacles.create(630, 365, 'obstacle');
+  obstacles.create(649, 365, 'obstacle');
+  obstacles.create(678, 365, 'obstacle');
+  obstacles.create(660, 365, 'obstacle');
+  obstacles.create(679, 365, 'obstacle');
+  obstacles.create(708, 365, 'obstacle');
+
+
+  /*======================
+        COLLIDERS
+  ========================*/
   this.physics.add.collider(player, obstacles, hitObstacles, null, this);
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(player, walls);
@@ -106,7 +137,9 @@ function create() {
         /CREATE WORLD
   ========================*/
 
-  // Create Animations
+  /*======================
+  CREATE CHARACTER ANIMATION
+  ========================*/
   this.anims.create({
     key: 'idleRight',
     frameRate: 4,
@@ -223,80 +256,73 @@ function create() {
       zeroPad: 4,
     }),
   });
+  // CREATE CONTROLL KEYS
   cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
+
+  /*======================
+      PLAYER MOVEMENT
+  ========================*/
   player.setVelocityX(0);
 
   if (playerDmg !== true) {
-    
-    // Player movement
-    if (cursors.right.isDown) 
-    {
+
+    if (cursors.right.isDown) {
       player.setVelocityX(160);
       player.anims.play('walkRight', true);
       facing = 'right';
     }
-    else if (cursors.left.isDown) 
-    {
+    else if (cursors.left.isDown) {
       player.setVelocityX(-160);
-      
+
       player.anims.play('walkLeft', true);
       facing = 'left';
-      
+
     }
-    else 
-    {
-      if (facing == 'left') 
-      {
+    else {
+      if (facing == 'left') {
         player.anims.play('idleLeft', true)
       }
-      else 
-      {
+      else {
         player.anims.play('idleRight', true)
       }
     }
-    if (cursors.up.isDown && player.body.touching.down) 
-    {
-      player.setVelocityY(-320);
-      
-      if (facing == 'left') 
-      {
+    if (cursors.up.isDown && player.body.touching.down) {
+      player.setVelocityY(-520);
+
+      if (facing == 'left') {
         // player.anims.play('jumpLeft', true)
       }
-      else 
-      {
+      else {
         // player.anims.play('jumpRight', true)
       }
     }
   }
-  else 
-  {
-    setTimeout(function () 
-    {
+  else {
+    setTimeout(function () {
+
       player.setVelocityY(-240);
       player.setTint();
       playerDmg = false;
+
     }, 500);
+
     player.setTint(0xff0000);
-    if (facing === 'right') 
-    {
+
+    if (facing === 'right') {
       player.anims.play('hurtRight', true);
     }
-    else
-    {
+    else {
       player.anims.play('hurtLeft', true);
-
     }
 
   }
 }
-function hitObstacles(player) 
-{
-  if (playerDmg !== true) 
-  {
+
+function hitObstacles(player) {
+  if (playerDmg !== true) {
     playerDmg = true;
   }
 }
-  
