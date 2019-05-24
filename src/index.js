ed44ea7299239936d3f060685e27ccf92514f8b4
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-05-24 19:09:16 +0200

    Spikes placed out on the course

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import PlatformImg from './assets/World/Platforms/platform.png';
 import RoofImg from './assets/World/Roofs/roof.png';
 import GroundImg from './assets/World/Platforms/ground.png';
 import Background5 from './assets/World/Background/parallax-mountain-bg.png';
-import ObstacleC from './assets/World/Obstacles/Spikes/spikeC.png'
+import Spikes from './assets/World/Obstacles/Spikes/spikeC.png'
+import FullRoomSpikes from './assets/World/Obstacles/Spikes/FullRoomSpikes.png'
 import WallImg from './assets/World/Walls/wall.png'
 
 const config = {
@@ -35,34 +36,39 @@ let facing = 'right';
 let platforms;
 let roofs
 let cursors;
-let obstacles;
+let spikes;
 let walls
 let playerDmg = false;
 
 
 function preload() {
+
+  /*======================
+      LOAD IN RESOURCES
+  ========================*/
   this.load.image('background-5', Background5);
   this.load.atlas('player', Hero, HeroAtlas);
   this.load.image('ground', GroundImg);
   this.load.image('platform', PlatformImg);
-  this.load.image('obstacle', ObstacleC);
+  this.load.image('spikes', Spikes);
+  this.load.image('full-room-spikes', FullRoomSpikes);
   this.load.image('wall', WallImg)
   this.load.image('roof', RoofImg)
 }
 
 function create() {
 
-  // Create background
+  // BACKGROUND PICTURE
   this.add.image(250, 260, 'background-5').setScale(3.5);
 
-  // Create player
-  // player = this.physics.add.sprite(100, 496, 'player', 'Idle/0001.png').setScale(.7);
-  player = this.physics.add.sprite(2072, 80, 'player', 'Idle/0001.png').setScale(.7);
+  // CREATE PLAYER
+  player = this.physics.add.sprite(100, 496, 'player', 'Idle/0001.png').setScale(.7);
+  // player = this.physics.add.sprite(1748, 180, 'player', 'Idle/0001.png').setScale(.7);
 
   player.setCollideWorldBounds(true);
   this.cameras.main.startFollow(player, true, 0.08, 0.1);
 
-  // Set world Bounds
+  // WORLD BOUNDRIES
   this.physics.world.setBounds(-220, -60, 30392, 6000); // Bounds expand downwards, counter that by moving Y accordingly
 
 
@@ -71,7 +77,7 @@ function create() {
   ========================*/
   platforms = this.physics.add.staticGroup();
   walls = this.physics.add.staticGroup();
-  obstacles = this.physics.add.staticGroup();
+  spikes = this.physics.add.staticGroup();
   roofs = this.physics.add.staticGroup();
 
   /*======================
@@ -97,7 +103,6 @@ function create() {
   /*======================
           FIRST ROOM
   ========================*/
-
   platforms.create(150, 400, 'platform').setScale(.3).refreshBody();
   platforms.create(30, 240, 'platform').setScale(.3).refreshBody();
 
@@ -106,23 +111,12 @@ function create() {
   walls.create(180, 385, 'wall').setScale(.4, 1).refreshBody();
   walls.create(180, 185, 'wall').setScale(.4, .5).refreshBody();
 
-  obstacles.create(220, 365, 'obstacle');
-  obstacles.create(249, 365, 'obstacle');
-  obstacles.create(278, 365, 'obstacle');
-  obstacles.create(400, 365, 'obstacle');
-  obstacles.create(429, 365, 'obstacle');
-  obstacles.create(458, 365, 'obstacle');
-  obstacles.create(630, 365, 'obstacle');
-  obstacles.create(649, 365, 'obstacle');
-  obstacles.create(678, 365, 'obstacle');
-  obstacles.create(660, 365, 'obstacle');
-  obstacles.create(679, 365, 'obstacle');
-  obstacles.create(708, 365, 'obstacle');
+
+
 
   /*======================
-  SECOND ROOM 
+        SECOND ROOM 
   ========================*/
-
   platforms.create(314, 390, 'platform').setScale(1, .4).refreshBody();
   platforms.create(610, 390, 'platform').setScale(1, .4).refreshBody();
   platforms.create(500, 150, 'platform').setScale(1, .4).refreshBody();
@@ -131,10 +125,22 @@ function create() {
   walls.create(745, 385, 'wall').setScale(.4, .5).refreshBody();
   walls.create(745, 85, 'wall').setScale(.4, .5).refreshBody();
 
-  /*======================
-  THIRD ROOM
-  ========================*/
+  spikes.create(220, 365, 'spikes');
+  spikes.create(249, 365, 'spikes');
+  spikes.create(278, 365, 'spikes');
+  spikes.create(400, 365, 'spikes');
+  spikes.create(429, 365, 'spikes');
+  spikes.create(458, 365, 'spikes');
+  spikes.create(630, 365, 'spikes');
+  spikes.create(649, 365, 'spikes');
+  spikes.create(678, 365, 'spikes');
+  spikes.create(660, 365, 'spikes');
+  spikes.create(679, 365, 'spikes');
+  spikes.create(708, 365, 'spikes');
 
+  /*======================
+        THIRD ROOM
+  ========================*/
   platforms.create(1080, 310, 'platform').setScale(1, .4).refreshBody();
   platforms.create(910, 150, 'platform').setScale(1, .4).refreshBody();
   platforms.create(1210, 450, 'platform').setScale(1, .4).refreshBody();
@@ -145,32 +151,48 @@ function create() {
   walls.create(1445, 155, 'wall').setScale(.4, 1.13).refreshBody();
   walls.create(1345, 500, 'wall').setScale(.4, .25).refreshBody();
 
+
+  spikes.create(899, 522, 'spikes');
+  spikes.create(929, 522, 'spikes');
+  spikes.create(956, 522, 'spikes');
+  spikes.create(1129, 285, 'spikes');
+
+
+
   /*======================
-  FOURTH ROOM
+        FOURTH ROOM
   ========================*/
   platforms.create(1530, 470, 'platform').setScale(.3).refreshBody();
   platforms.create(1730, 470, 'platform').setScale(.3).refreshBody();
   platforms.create(2065, 470, 'platform').setScale(.3).refreshBody();
   platforms.create(2265, 310, 'platform').setScale(.3).refreshBody();
-  platforms.create(2600, 470, 'platform').setScale(.3).refreshBody();
+  platforms.create(2590, 470, 'platform').setScale(.3).refreshBody();
   platforms.create(2730, 310, 'platform').setScale(.3).refreshBody();
   platforms.create(2630, 150, 'platform').setScale(.3).refreshBody();
   platforms.create(2372, 90, 'platform').setScale(1, .3).refreshBody();
   platforms.create(2072, 120, 'platform').setScale(1, .3).refreshBody();
-  platforms.create(1748, 220, 'platform').setScale(.3).refreshBody();
+  platforms.create(1718, 220, 'platform').setScale(.4, .3).refreshBody();
 
   walls.create(1650, 235, 'wall').setScale(.4, .6).refreshBody();
   walls.create(2785, 385, 'wall').setScale(.4, 1).refreshBody();
   walls.create(2785, 135, 'wall').setScale(.4, 1).refreshBody();
 
+  spikes.create(1985, 522, 'full-room-spikes');
+  spikes.create(2620, 522, 'spikes');
+  spikes.create(2650, 522, 'spikes');
+  spikes.create(2680, 522, 'spikes');
+  spikes.create(2710, 522, 'spikes');
+  spikes.create(2740, 522, 'spikes');
+  spikes.create(2755, 522, 'spikes');
+
   /*======================
         COLLIDERS
   ========================*/
-  this.physics.add.collider(player, obstacles, hitObstacles, null, this);
+
+  this.physics.add.collider(player, spikes, hitObstacles, null, this);
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(player, walls);
   this.physics.add.collider(player, roofs);
-
   /*======================
         /CREATE WORLD
   ========================*/
